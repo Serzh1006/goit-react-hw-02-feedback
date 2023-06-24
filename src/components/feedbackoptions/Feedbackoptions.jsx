868377eb05aadf58f0f7125ae731d77e673b97d3
Serzh1006 <skravchenko223@gmail.com>
2023-06-25 01:11:31 +0200

    task-1

---
+++ src/components/feedbackoptions/Feedbackoptions.jsx
@@ -0,0 +1,15 @@
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(option => {
+    return (
+      <button
+        key={option}
+        type="button"
+        onClick={() => onLeaveFeedback(option)}
+      >
+        {option}
+      </button>
+    );
+  });
+};
+
+export default FeedbackOptions;
